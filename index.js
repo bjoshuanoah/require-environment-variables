8c28c4057e76b3a8e158f2c95f7c650dbffb30c3
Brian Noah <brianjoshuanoah@gmail.com>
2018-10-03 10:42:43 -0500

    update travis requirements, update build, return object of env var values

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 'use strict';
 
 module.exports = function (variableArray) {
+    var envArr = {};
+
 	if (!Array.isArray(variableArray)) {
 		console.error('Array required');
-		return;
+		return {};
 	}
 	var missingVariables = [];
 	for (var i = 0; i < variableArray.length; i++) {
 		if (!process.env[variableArray[i]]) {
 			missingVariables.push(variableArray[i]);
+            continue;
 		}
+        envArr[variableArray[i]] = process.env[variableArray[i]];
 	}
 	if (missingVariables.length) {
 		console.error('Environment variables needed:', JSON.stringify(missingVariables));
 		throw new Error('Environment variables missing');
 	}
+    return envArr;
 };
